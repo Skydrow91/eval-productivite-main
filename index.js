8f27fd06ef08d7926b291b290250a2b385cd7f3a
Skydrow91 <64668442+Skydrow91@users.noreply.github.com>
2022-01-30 21:31:33 +0100

    Documenter le Code

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
+//Importation des librairies
+
 import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url'
 import cors from 'cors'
 import Sanitizer from 'sanitizer';
 
+/**
+ * @param  {} import.meta.url
+ * @param  {} constdirname=path.dirname(filename
+ */
+
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 const port = 8070
 const host = '127.0.0.1'
+
+/**
+ * @param  {true}} app.use(express.urlencoded({extended
+ */
 
 const app = express()
 app.use(express.urlencoded({ extended: true }))
@@ -15,11 +26,32 @@ app.use(cors())
 app.use(express.static(path.join(dirname, 'public')))
 app.use('/favicon.ico', express.static(path.join(dirname, 'public', 'images', 'favicon.png')))
 
+/**
+ * @param  {} '/'
+ * @param  {} (req
+ * @param  {} res
+ * @param  {} =>{res.sendFile('index.html'
+ * @param  {path.join(dirname} {root
+ */
+
 app.get('/', (req, res) => {
   res.sendFile('index.html', { root: path.join(dirname) }, err => {
     if (err) throw new Error(err)
   })
 })
+
+/**
+ * @param  {} '/comment'
+ * @param  {} (req
+ * @param  {} res
+ * @param  {} =>{constcomment=Sanitizer.sanitize(req.body.message
+ * @param  {} ;commentif(comment
+ * @param  {} {returnres.status(200
+ * @param  {} .send(comment
+ * @param  {} }res.status(401
+ * @param  {} .send('Accèsinterdit!'
+ * @param  {} }
+ */
 
 app.post('/comment', (req, res) => {
  const comment = Sanitizer.sanitize(req.body.message);
