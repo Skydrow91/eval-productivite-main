99fd3c8f1962daa8061aa75077799d8d46035615
Skydrow91 <64668442+Skydrow91@users.noreply.github.com>
2022-01-31 08:09:37 +0100

    Linter le code/Embellir le code

--- public/js/functionsToTest.js
+++ public/js/functionsToTest.js
@@ -1,35 +1,38 @@
-/**
- * @function returnAnObject
- * @param  {string} letresponse
- * @param  {string} letindex
- */
+ /**
+   * @function returnAnObject {object} - Fonction avec un argument
+   * @param {object} - Condition d'exécution de la fonction
+   * @param {number} - valeur par défaut
+   * @param {Array} - Argument attendu
+   */
 
 const returnAnObject = (...args) => {
   let response = {};
   if (args.length) {
     let index = 0;
-    args.forEach(arg => {
+    args.forEach((arg) => {
       response[index] = arg;
       index++;
-    })
+    });
   } else {
-    response = 'No argument was given to the function.';
+    response = "No argument was given to the function.";
   }
   return response;
 };
 
-/**
- * @function multiplyAllByTwo
- * @param  {number} letresponse
- */
-
+ /** @function multiplyByTwo {object} - Fonction avec un Objet
+   * @param {number} - Element de l'objet
+   * @param {constructor} - Condition d'exécution de la fonction
+   * @param {number} - Résultat de la fonction
+   */
 const multiplyAllByTwo = (arrayOfNumbers) => {
   let response;
-  if (arrayOfNumbers.constructor.prototype === new Array([]).constructor.prototype) {
-    response = arrayOfNumbers.map((val => val * 2));
-    console.log('arrayTimesTwo: ', arrayTimesTwo);
+  if (
+    arrayOfNumbers.constructor.prototype === new Array([]).constructor.prototype
+  ) {
+    response = arrayOfNumbers.map((val) => val * 2);
+    console.log("arrayTimesTwo: ", arrayTimesTwo);
   } else {
-    response = 'The argument is not an Array of numbers';
+    response = "The argument is not an Array of numbers";
   }
   return response;
 };
